944c240112f098a7b10a17c12be3d0aad78a4aa7
Oleh0293 <oleh.kulyk.ik.2021@lpnu.ua>
2021-09-22 17:20:18 +0300

    second_vitka_change

--- Lab_02.cpp
+++ Lab_02.cpp
@@ -13,6 +13,7 @@ int main() {
     double y; // результат обчислення 2-го виразу
     int zmina;
     int sun;
+    int jony;
     cout << "y=";cin >> y;
     cout << "x=";cin >> x;
     z1 = cos(x)*cos(x)*cos(x)*cos(x) + sin(y)*sin(y) + 1./4 * sin(2*x)* sin(2*x) -1;
